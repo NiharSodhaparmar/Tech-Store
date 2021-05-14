1c6eb4e66017c4d869999553bb4c7e11166004aa
NiharSodhaparmar <niharsodha17@gmail.com>
2021-05-14 19:51:01 +0530

    remove unused variables

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -6,7 +6,7 @@ export default function Sidecart() {
   return (
     <ProductConsumer>
       {(value) => {
-        const { cartOpen, closeCart, cart } = value;
+        const { cartOpen, closeCart } = value;
         return (
           <CartWrapper show={cartOpen} onClick={closeCart}>
             <p>cart items</p>
